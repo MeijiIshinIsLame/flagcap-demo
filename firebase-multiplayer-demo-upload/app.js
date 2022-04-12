67c3433dff7f408a8b8f3a93b344e9c20ee54aae
Zachary Silva <zsilva@honolulucookie.local>
2022-04-11 16:59:51 -1000

    idk

--- firebase-multiplayer-demo-upload/app.js
+++ firebase-multiplayer-demo-upload/app.js
@@ -13,6 +13,35 @@ function getKeyString(x, y) {
   let playerId;
   let playerRef;
 
+  function initGame() {
+    const allPlayersRef = firebase.database.ref(`players`);
+    const allCoinsRef = firebase.database.ref(`coins`);
+
+    allPlayersRef.on("value", (snapshot) => {
+      //fires on change
+    })
+    allPlayersRef.on("child_added", (snapshot) => {
+      //whenever new node is added
+      const addedPlayer = snapshot.val();
+      const characterElement = document.createElement("div");
+      const characterElement.classList.add("Character", "grid-cell");
+      if (addedPlayer.id == playerId) {
+        chatacterElement.classList.add("you");
+      }
+
+      characterElement.innerHTML = ( `
+        <div class="Character_shadow grid-cell"></div>
+        <div class="Character_sprite grid-cell"></div>
+        <div class="Character_name-container">
+          <span class="Character_name"></span>
+          <span class="Character_coins"></span>
+        </div>
+        <div class="Character_you-arrow"></div>
+      `);
+
+    })
+  }
+
   firebase.auth().onAuthStateChanged((user) => {
     console.log(user.uid)
     if (user) {
@@ -30,6 +59,8 @@ function getKeyString(x, y) {
       })
 
       playerRef.onDisconnect().remove();
+
+      initGame();
     }
   })
 
